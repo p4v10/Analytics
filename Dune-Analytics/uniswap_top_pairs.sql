f3a0ccb424e54ddd15251813653ff1ad18b69ba3
Pavlo <71569680+p4v10@users.noreply.github.com>
2023-07-08 22:37:34 -0500

    Update uniswap_top_pairs.sql

--- Dune-Analytics/uniswap_top_pairs.sql
+++ Dune-Analytics/uniswap_top_pairs.sql
@@ -1,5 +1,5 @@
 /*
-In this query we will look at the top 5 pairs by volume on Uniswap starting from Jan 1, 2023
+In this query, we will look at the top 5 pairs by volume on Uniswap starting from Jan 1, 2023
 
 Dune Query - https://dune.com/queries/2701741
 
@@ -7,7 +7,7 @@ Last edited: 07.08.2023
 
 */
 
--- get the list of top 5 traded pairs excluding stablescoins
+-- Get the list of top 5 traded pairs excluding stablecoins
 WITH most_traded_pairs AS (
     SELECT DISTINCT
         token_pair, -- like USDC-WETH
@@ -15,7 +15,7 @@ WITH most_traded_pairs AS (
         SUM(token_bought_amount) AS amount_of_tokens_bought, -- amount of token bought, e.g 1.37 of WETH
         SUM(amount_usd) AS usd_value_tokens -- amount in USD dollars
     FROM dex.trades -- trades on decentralized exchanges table
-    WHERE blockchain = 'ethereum'
+    WHERE blockchain = 'ethereum' 
     AND project = 'uniswap' -- we are only going to look at uniswap protocol on ethereum network
     AND block_date >= DATE('2023-01-01') -- convert date to string and filter for beginning of 2023
     GROUP BY token_pair
@@ -23,9 +23,9 @@ WITH most_traded_pairs AS (
     LIMIT 6
 )
 SELECT
-    DATE_TRUNC('week', dx.block_date) AS week_date, -- get the week date
+    SPLIT_PART(CAST(DATE_TRUNC('week', dx.block_date) AS VARCHAR), ' ', 1) AS week_date, -- get the week start date in a string format
     tp.token_pair,
-    SUM(dx.amount_usd) AS total_weekly_volume -- weekly volume in USD
+    ROUND(SUM(dx.amount_usd), 0) AS total_weekly_volume -- weekly volume in USD
 FROM dex.trades AS dx
 INNER JOIN most_traded_pairs AS tp -- join our top 5 pairs
     ON dx.token_pair = tp.token_pair
